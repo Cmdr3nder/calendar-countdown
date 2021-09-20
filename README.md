87049fec5cba7e5792bda3f6c40b23cd7f00af85
Andrew Bowers <andrew.bowers@agamecoder.com>
2021-09-19 22:28:11 -0700

    Started in on calculating which Haloween to use

--- README.md
+++ README.md
@@ -1,11 +1,6 @@
-# React Project Template
+# Calendar Countdown
 
-## Usage
-1. Clone this repo `git clone https://github.com/Cmdr3nder/react-spa-template.git your-project-name`
-1. Enter the new project directory `cd your-project-name`
-1. Delete the *.git/* folder `rm -rf .git`
-1. Initialize a fresh git repo `git --init`
-1. Make your initial commit `git add . && git commit -m "INITIAL COMMIT"`
+A simple React based SPA that counts down to Haloween
 
 ## Prerequisites
 * node ^16.9.1
@@ -23,5 +18,3 @@ With `make` (preferred):
 	* In order to stop `make watch-build` you will need to double-tap CTRL-C twice in relatively rapid succession.
 1. Suggestion: Run `make serve` and `make watch-build` simultaneously to achieve a tight development loop
 
-Without `make`:
-TBD
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+	DateTime,
+	Interval,
+} from 'luxon';
 
 import WelcomeMessage from './welcome';
 
+function findHaloweenForYear(year) {
+	const start = DateTime.local(year, 10, 31).startOf('day');
+
+	return Interval.fromDateTimes(
+		start,
+		start.endOf('day')
+	);
+}
+
+function nextHoliday(from, findHolidayForYear) {
+	const year = from.toLocal().year;
+	let holiday = findHolidayForYear(year);
+
+   // If holiday is occurring during 'from' or is after 'from'
+	if (!holiday.isBefore(from)) {
+		return holiday;
+	}
+
+	return findHolidayForYear(year + 1);
+}
+
 function App() {
+	const nextHaloween = React.useMemo(() => nextHoliday(DateTime.now(), findHaloweenForYear), []);
+	const [ currently, setCurrently ] = React.useState(DateTime.now());
+
+	React.useEffect(() => {
+		const intervalID = setInterval(() => {
+			setCurrently(DateTime.now());
+		}, 0.5);
+
+		return () => clearInterval(intervalID);
+	}, []);
+
 	return (
-		<WelcomeMessage />
+		<>
+			<WelcomeMessage />
+			<div>{currently.toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS)}</div>
+			<div>{nextHaloween.toLocaleString()}</div>
+		</>
 	);
 }
 
